db555667f5ac326842edf63df179d9ca8d4c6b64
Julian Gan <juliangan07@gmail.com>
2017-03-23 22:15:17 -0500

    Binder
    Ongoing project for my Senior Capstone for Information Systems at the
    University of North Dakota

---
+++ Binder - Master/js/script.js
@@ -0,0 +1,12 @@
+$(function() {
+    $('[data-toggle="tooltip"]').tooltip();
+    
+    $('[data-toggle="popover"]').popover();
+
+    $('.carousel').carousel({
+        interval: 2000,
+        pause: false,
+        wrap: false,
+        keyboard: false
+    });
+});
